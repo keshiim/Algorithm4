e8b8931bc06c70c01a8f31671fa6817df85b4ba0
zhengmingjun <keshiim@163.com>
2017-06-29 14:51:51 +0800

    最大公约数

---
+++ GCD.java
@@ -0,0 +1,14 @@
+/* 求最大公约数 */
+class GCD {
+	//计算
+	public static int gcd(int p, int q) {
+		if (q == 0)
+			return p;
+		int r = p % q;
+		return gcd(q, r);
+	}
+	
+	public static void main(String[] args) {
+		System.out.println(gcd(5, 2));
+	}
+}
